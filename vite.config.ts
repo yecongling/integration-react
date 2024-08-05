f59d41ac9dab46b7b1a0d1e09f7b1683e5085aaf
ycl <499475142@qq.com>
2024-08-05 10:15:51 +0800

    配置mock在生产环境下不启用

--- vite.config.ts
+++ vite.config.ts
@@ -4,40 +4,42 @@ import path from "path";
 import { viteMockServe } from "vite-plugin-mock";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    viteMockServe({
-      mockPath: "./mock/",
-      logger: true,
-      enable: true
-    }),
-  ],
-  // 配置路径别名解析
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [
+      react(),
+      viteMockServe({
+        mockPath: "./mock/",
+        logger: true,
+        enable: command === "serve", // 生产环境不启用
+      }),
+    ],
+    // 配置路径别名解析
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+      extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
     },
-    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
-  },
-  // css预处理器
-  css: {
-    preprocessorOptions: {
-      scss: {
-        // 引入全局变量
-        additionalData: `@import "@/assets/styles/variables.scss";`,
+    // css预处理器
+    css: {
+      preprocessorOptions: {
+        scss: {
+          // 引入全局变量
+          additionalData: `@import "@/assets/styles/variables.scss";`,
+        },
       },
     },
-  },
-  // 服务器配置以及代理
-  server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "http://localhost:8080/integration",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+    // 服务器配置以及代理
+    server: {
+      port: 3000,
+      proxy: {
+        "/api": {
+          target: "http://localhost:8080/integration",
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
       },
     },
-  },
+  };
 });
